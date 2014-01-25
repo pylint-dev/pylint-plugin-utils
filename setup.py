90b99bbfdfb03bf45121f9d963d9b4b53d575e3e
carlio <git@carlcrowder.com>
2014-01-25 13:56:21 +0100

    Removing now unecessary time import

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # -*- coding: UTF-8 -*-
 from distutils.core import setup
 from setuptools import find_packages
-import time
 
 _version = "0.1.1"
 _packages = find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"])
