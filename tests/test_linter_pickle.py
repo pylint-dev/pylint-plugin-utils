a3ef0666fcb70ea3f4902af9606ae35420c71bc8
Carl Crowder <carl@zerfeciz.com>
2023-05-14 19:42:32 +0700

    The test can stay for older versions of pylint, but probably is not necessary now since pylint is up to 2.17

--- tests/test_linter_pickle.py
+++ tests/test_linter_pickle.py
@@ -1,5 +1,6 @@
 import pickle
 
+import pylint
 from pylint.checkers.typecheck import TypeChecker
 
 from pylint_plugin_utils import augment_visit, suppress_message
@@ -14,18 +15,18 @@ def fake_suppress_func(*args, **kwargs):
 
 
 def test_linter_should_be_pickleable(linter):
+    # after pylint>=2.13, dill is used in pylint -
+    # see discussion https://github.com/PyCQA/pylint-plugin-utils/issues/26
+    # therefore we can ignore that test, as the previous reasons for this
+    # test no longer exist
+    # (reason was https://github.com/PyCQA/pylint-plugin-utils/issues/20)
+    if pylint.__version__ >= "2.13":
+        return
+
     # Setup
-    linter.register_checker(TypeChecker())
+    linter.register_checker(TypeChecker(linter))
     augment_visit(linter, TypeChecker.visit_attribute, fake_augmentation_func)
     suppress_message(linter, TypeChecker.visit_attribute, "no-member", fake_suppress_func)
 
     # Act and Assert
-    try:
-        pickle.dumps(linter)
-    except TypeError:
-        # this can happen for newer versions of pylint (>2.12) which now use dill for pickling
-        import dill
-
-        # this import will fail for earlier versions which do not have dill as a dependency,
-        # in which case this is an old version which also does not pickle
-        dill.dumps(linter)
+    pickle.dumps(linter)
